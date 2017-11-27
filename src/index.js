6661a32f823533c2773c481160dd3dc7d3eedd16
spencerkingman <spencerkingman@gmail.com>
2017-11-26 21:37:58 -0500

    Add a linter

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome couse!`);
+console.log(`I would pay ${courseValue} for this awesome couse!`); // eslint-disable-line no-console
+
